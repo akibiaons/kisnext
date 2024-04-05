a7a2340519a31159888edefa5ffacacfbda22496
Arthur Fernandez <arthur@snooget.com>
2024-04-05 14:35:48 -0700

    feat: Hero section layout
    - Add hero section text layout, need background.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
 export default function Home() {
   return (
     <>
-      {/*  */}
-      {/*  */}
+      <div className="container mx-auto">
+        <h1>
+          The body of the website below will be a billboard for a hero section
+        </h1>
+        {/* The background image for the hero section */}
+        <div className="bg-[url('/path/to/your/image.jpg')] bg-cover bg-center h-[500px] flex items-center justify-center text-white">
+          <div className="text-center">
+            <h2>Keep it Safe California</h2>
+            <p>High quality American Security & Cannon Safes</p>
+            <a href=""></a>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
